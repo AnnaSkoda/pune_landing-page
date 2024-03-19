ab607f2729b366b4561dc5c6a3e8d554df3924f9
AnnaSkoda <zbaraz77@gmail.com>
2024-03-19 22:30:44 +0200

    adaptive footer

--- js/main.js
+++ js/main.js
@@ -9,20 +9,20 @@ for (let anchor of anchors) {
             block: "start"
         })
     });
-
-    $(".mobile-menu").click(function () {
-        $(".mobile-menu span").toggleClass('active');
-        $(".nav").toggleClass('open');
-        $(".menu").toggleClass('active');
-        $("body").toggleClass('owerlay');
-    });
-
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 99) {
-            $('.header-top').addClass('header-top__fixed')
-        } else {
-            $('.header-top').removeClass('header-top__fixed')
-        }
-    });
 };
+
+$(".mobile-menu").click(function (e) {
+    $(".mobile-menu span").toggleClass('active');
+    $(".nav").toggleClass('open');
+    $(".menu").toggleClass('active');
+    $("body").toggleClass('owerlay');
+});
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 99) {
+        $('.header-top').addClass('header-top__fixed')
+    } else {
+        $('.header-top').removeClass('header-top__fixed')
+    }
+});
 
